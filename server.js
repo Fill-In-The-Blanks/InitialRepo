bea704b54ce35a6563b4a2e5a8c4102bd01e9b68
Kilamalik <maliksamath11@gmail.com>
2022-04-04 12:08:52 +0530

    Setup for deployment

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const path = require('path');
 
 const app = express();
 
-// app.get('/', (req, res) => res.send('API Running'));
+//app.get('/', (req, res) => res.send('API Running'));
 
-    // server static assets in production
+// server static assets in production
 if (process.env.NODE_ENV === 'production') {
     // set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static('frontend/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
 }
 
 const PORT = process.env.PORT || 5000;
