e8050d45d5ebadd459969206b17140fa70ce2119
Kilamalik <maliksamath11@gmail.com>
2022-04-06 23:07:34 +0530

    Index page navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Hello World! from group SD13.
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Fragment>
+    <Navbar />
+  </Fragment>
+);
 
 export default App;
