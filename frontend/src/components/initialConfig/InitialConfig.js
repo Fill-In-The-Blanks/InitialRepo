fcc90251c48a5fe5fa66b2561f06d6205e0e988b
Kilamalik <maliksamath11@gmail.com>
2022-05-02 13:20:01 +0530

    Read excel sheet into frontend

---
+++ frontend/src/components/initialConfig/InitialConfig.js
@@ -0,0 +1,78 @@
+import React, { Fragment, useState } from 'react';
+import * as XLSX from 'xlsx';
+
+const InitialConfig = () => {
+  const [formData, setFormData] = useState([]);
+
+  // Sam Lama's code - https://github.com/Rinlama/ReactTools/blob/readexcel/src/App.js
+  // YouTube - https://www.youtube.com/watch?v=h33eakwu7oo
+  const readExcel = (file) => {
+    const promise = new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
+
+      fileReader.onload = (e) => {
+        const bufferArray = e.target.result;
+
+        const wb = XLSX.read(bufferArray, { type: 'buffer' });
+
+        const wsname = wb.SheetNames[0];
+
+        const ws = wb.Sheets[wsname];
+
+        const data = XLSX.utils.sheet_to_json(ws);
+
+        resolve(data);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+
+    promise.then((d) => {
+      /* console.log(d); */
+      setFormData(d);
+    });
+  };
+
+  return (
+    <Fragment>
+      <h2>Inital Configuration</h2>
+      Upload Instructor File <br></br>
+      <input
+        type='file'
+        onChange={(e) => {
+          const file = e.target.files[0];
+          readExcel(file);
+        }}
+      />
+      <table className='table'>
+        <thead>
+          <tr>
+            <th scope='col'>Employee No.</th>
+            <th scope='col'>Employee Name</th>
+            <th scope='col'>Employee Email</th>
+            <th scope='col'>Phone</th>
+            <th scope='col'>Specialization</th>
+            <th scope='col'>Vacancy Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {formData.map((d) => (
+            <tr key={d.empNo}>
+              <td>{d.empNo}</td>
+              <td>{d.empName}</td>
+              <td>{d.sliitEmail}</td>
+              <td>{d.phone}</td>
+              <td>{d.department}</td>
+              <td>{d.vacancyStatus}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+export default InitialConfig;
