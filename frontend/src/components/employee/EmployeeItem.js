3eb188f61f0ec2b2fb1356f69ef09b305d1d050a
Kilamalik <maliksamath11@gmail.com>
2022-05-17 09:50:34 +0530

    Retrieval of employees

---
+++ frontend/src/components/employee/EmployeeItem.js
@@ -0,0 +1,57 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+const EmployeeItem = ({ employees }) => {
+  const employeesMapped = employees.map((employee) => (
+    <tr key={employee._id}>
+      <td>{employee.empNo}</td>
+      <td>{employee.empName}</td>
+      <td>{employee.sliitEmail}</td>
+      <td>{employee.phone}</td>
+      <td>{employee.department}</td>
+      <td>{employee.vacancyStatus}</td>
+      <td>
+        {' '}
+        <button className='btn btn-danger'>Delete </button>
+      </td>
+    </tr>
+  ));
+  return (
+    <Fragment>
+      {/* <p className='lead'> Module Management</p> */}
+      <table className='table'>
+        <thead>
+          <tr>
+            <th className='hide-sm' style={{ textAlign: 'center' }}>
+              Employee Number
+            </th>
+            <th className='hide-sm' style={{ textAlign: 'center' }}>
+              Name
+            </th>
+            <th className='hide-sm' style={{ textAlign: 'center' }}>
+              SLIIT Email
+            </th>
+            <th className='hide-sm' style={{ textAlign: 'center' }}>
+              Phone Number
+            </th>
+            <th className='hide-sm' style={{ textAlign: 'center' }}>
+              Department
+            </th>
+            <th className='hide-sm' style={{ textAlign: 'center' }}>
+              Vacancy Status
+            </th>
+          </tr>
+        </thead>
+        <tbody>{employeesMapped}</tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+EmployeeItem.propTypes = {
+  employees: PropTypes.array.isRequired,
+};
+
+export default connect(null, {})(EmployeeItem);
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,12 +2,13 @@ import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
 import module from './module';
-
+import employee from './employee';
 
 export default combineReducers({
   alert,
   auth,
-  module
+  module,
+  employee,
 });
 
 // this gets imported in the store.js file
