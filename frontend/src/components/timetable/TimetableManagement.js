fe9de0353a7cc95118bacb287977844092d536c9
Kilamalik <maliksamath11@gmail.com>
2022-05-23 15:46:07 +0530

    Slots bulk delete confirmation box

--- frontend/src/components/timetable/TimetableManagement.js
+++ frontend/src/components/timetable/TimetableManagement.js
@@ -3,16 +3,18 @@ import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { getSlots, deleteSlots } from '../../actions/timetable';
+import { getSlots } from '../../actions/timetable';
 import TimetableItem from './TimetableItem';
+import SlotsConfirmationDialog from '../dialogBox/SlotsConfirmationDialog';
 
-const TimetableManagement = ({
-  getSlots,
-  deleteSlots,
-  timetable: { slots },
-}) => {
+const TimetableManagement = ({ getSlots, timetable: { slots } }) => {
+  const [buttonStatus, setButtonStatus] = useState({
+    delete: false,
+  });
+
   useEffect(() => {
     getSlots();
+    buttonStatus.delete = false;
   }, []);
 
   return (
@@ -20,27 +22,40 @@ const TimetableManagement = ({
       <section className='container container-margin-top-override'>
         <p className='lead'>Timetable Management</p>
 
-        <Link to={`/`}>
-          <button className='btn btn-primary' style={{ marginBottom: '5px' }}>
-            Add Slot
-          </button>
-        </Link>
-        <Link to={`/timetableManagement`}>
-          <button className='btn btn-primary'>List Slots</button>
-        </Link>
-
-        <button
-          className='btn btn-danger'
-          style={{ float: 'right' }}
-          onClick={() => deleteSlots()}
-        >
-          Delete All Slots{' '}
-        </button>
-
-        {slots.length > 0 ? (
-          <TimetableItem slots={slots} />
+        {buttonStatus.delete ? (
+          <Fragment>
+            <SlotsConfirmationDialog
+              status={buttonStatus}
+              setStatus={setButtonStatus}
+              deleteP={buttonStatus.delete}
+            />
+          </Fragment>
         ) : (
-          <h4>No slots found</h4>
+          <Fragment>
+            <Link to={`/`}>
+              <button
+                className='btn btn-primary'
+                style={{ marginBottom: '5px' }}
+              >
+                Add Slot
+              </button>
+            </Link>
+            <Link to={`/timetableManagement`}>
+              <button className='btn btn-primary'>List Slots</button>
+            </Link>
+            <button
+              className='btn btn-danger'
+              style={{ float: 'right' }}
+              onClick={() => setButtonStatus({ delete: !buttonStatus.delete })}
+            >
+              Delete All Slots{' '}
+            </button>
+            {slots.length > 0 ? (
+              <TimetableItem slots={slots} />
+            ) : (
+              <h4>No slots found</h4>
+            )}
+          </Fragment>
         )}
       </section>
     </Fragment>
@@ -49,10 +64,8 @@ const TimetableManagement = ({
 
 TimetableManagement.propTypes = {
   getSlots: PropTypes.func.isRequired,
-  deleteSlots: PropTypes.func.isRequired,
   timetable: PropTypes.object.isRequired,
   auth: PropTypes.object.isRequired,
-  Confirm: PropTypes.any,
 };
 
 const mapStateToProps = (state) => ({
@@ -60,6 +73,4 @@ const mapStateToProps = (state) => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, { getSlots, deleteSlots })(
-  TimetableManagement
-);
+export default connect(mapStateToProps, { getSlots })(TimetableManagement);
